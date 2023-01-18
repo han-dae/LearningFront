9ba8af6caf610df2f4781607403fe472d13dfd6f
ck <ck@naver.com>
2023-01-18 18:37:44 +0900

    0118

--- src/component/ApiService.js
+++ src/component/ApiService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const USER_API_BASE_URL = "http://localhost:8080/users";
+const USER_API_BASE_URL = "http://localhost:8081/users/users";
 
 class ApiService {
   fetchUser() {
@@ -10,8 +10,8 @@ class ApiService {
     return axios.get(USER_API_BASE_URL + "/" + userID);
   }
 
-  deleteUser(userID) {
-    return axios.delete(USER_API_BASE_URL + "/" + userID);
+  deleteUser(userID, user) {
+    return axios.delete(USER_API_BASE_URL + "/" + userID.id,user);
   }
   addUser(user) {
     return axios.post(USER_API_BASE_URL, user);
--- src/component/route/AppRouter.jsx
+++ src/component/route/AppRouter.jsx
@@ -3,6 +3,7 @@ import { Route, Routes, BrowserRouter} from "react-router-dom"
 import MainComponent from "../pageComponent/MainComponent";
 import Login from "../pageComponent/Login";
 import TestComponent from "../pageComponent/Test";
+import MainService1 from "../pageComponent/MainService1";
 
 const AppRouter =() => {
     return (
@@ -13,6 +14,10 @@ const AppRouter =() => {
             <Route path="/home" element={<MainComponent />} />
             <Route path="/login" element={<Login/>} />
             <Route path ="/test" element={<TestComponent />} />
+            <Route path ="/mainService1" element={<MainService1 />} />
+            <Route path ="/mainService2" element={<MainService2 />} />
+            <Route path ="/mainService3" element={<MainService3 />} />
+            <Route path ="/mainService4" element={<MainService4 />} />
         </Routes>
         </BrowserRouter></div>
     );
