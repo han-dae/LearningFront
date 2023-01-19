fe4245be4e53a46ae3467e32d4863996ac171f92
handaechan <h33223012@naver.com>
2023-01-19 15:58:21 +0900

    0119

--- src/component/pageComponent/Test.jsx
+++ src/component/pageComponent/Test.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Background from "./Background";
 import Category from "./Category";
 import MainHeader from "./Header";
 import ScrollBottom from "./ScrollBottom";
 import InputTextArea from "./TextArea";
-
+import axios from "axios";
 
 const TestComponent = () => {
+
+  saveFile = (e) => {
+    e.preventDefault();
+
+    let file ={
+      filename : this.state.filename,
+      fileUrl : this.state.fileUrl
+    }
+  }
+  ApiService.saveFile(file)
+  .then((res) => {
+    this.setFile({
+      filename : this.filename,
+      fileUrl : this.fileUrl
+    })
+    
+  })
+const fileShow = (props) =>{
   return (
-    <div >
+    <>
+    <div style={{backgroundImage : "{}"}}></div></>
+  )
+}
+
+  return (
+    <div>
+        <form>
       <ScrollBottom />
       <MainHeader />
       <Background />
@@ -28,8 +53,8 @@ const TestComponent = () => {
           style={{
             display: "inline-block",
             width: "100%",
-        
-            marginTop : "-10px"
+
+            marginTop: "-10px",
           }}
         >
           <h1>Chapter1. 스크립트 분석</h1>
@@ -37,40 +62,47 @@ const TestComponent = () => {
             높은 조회 수와 매출을 기록한 스크립트에요.
           </h3>
         </div>
-       
-          </div>
-          <div className="Chart01"
-            style={{
-              width: "35vw",
-              height: "40vh",
-              marginTop : "25px",
-              marginLeft : "20vw",
-              backgroundColor: "white",
-              overflowY: "scroll",
-              borderRadius : 10
-            }}
-          ></div>
-          <div className="Chart02"
-            style={{
-              marginTop: "25px",
-              marginLeft : "20vw",
-              width: "28vw",
-              height: "30vh",
-              backgroundColor: "black",
-            }}
-          >
-            
-          </div>
-          
-          <div className="InputCategory" style={{marginTop : "-710px", marginLeft:"57vw"}}>
-            <div style={{marginBottom : "15px"}}>
-            <Category/></div>
-            <InputTextArea/>
-            </div>
-      
-      
+      </div>
+      <div
+        className="Chart01"
+        style={{
+          width: "35vw",
+          height: "40vh",
+          marginTop: "25px",
+          marginLeft: "20vw",
+          backgroundColor: "white",
+          overflowY: "scroll",
+          borderRadius: 10,
+        }}
+      ></div>
+      <div
+        className="Chart02"
+        style={{
+          marginTop: "25px",
+          marginLeft: "20vw",
+          width: "28vw",
+          height: "30vh",
+          backgroundColor: "black",
+        }}
+      ></div>
+
+      <div
+        className="InputCategory"
+        style={{ marginTop: "-710px", marginLeft: "57vw" }}
+      >
+        <div style={{ marginBottom: "15px" }}>
+          <Category />
+        </div>
+        <InputTextArea />
+        <input type="file"
+        name="profile_files"
+        multiple="multiple"
+         />
+
+        <input type={"submit"}/>
+      </div>
+      </form>
     </div>
-    
   );
 };
 export default TestComponent;
--- src/component/route/AppRouter.jsx
+++ src/component/route/AppRouter.jsx
@@ -3,12 +3,13 @@ import { Route, Routes, BrowserRouter} from "react-router-dom"
 import MainComponent from "../pageComponent/MainComponent";
 import Login from "../pageComponent/Login";
 import TestComponent from "../pageComponent/Test";
-
+import Regist from "../pageComponent/SendVideo";
 const AppRouter =() => {
     return (
         <div>
       <BrowserRouter>
         <Routes>
+            <Route path="/test" element={<Regist/>} />
             <Route path="/" element={<MainComponent />} />
             <Route path="/home" element={<MainComponent />} />
             <Route path="/login" element={<Login/>} />
