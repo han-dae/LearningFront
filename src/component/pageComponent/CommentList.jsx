1d79e10673df08305f8d10ace2fc544c4081ea8a
hayeong <gkdud630@gmail.com>
2023-01-31 17:33:48 +0900

    yeong1733

--- src/component/pageComponent/CommentList.jsx
+++ src/component/pageComponent/CommentList.jsx
@@ -2,20 +2,25 @@ import React from 'react'
 
 const CommentList = ({ com }) => {
     console.log(com)
-    const smallText = {
+    const timeText = {
         color: 'gray',
         fonSize: '8px'
     }
-    
+    const idText = {
+        color:'black',
+        fontSize:'10px',
+        fontWeight:'bold'
+    }
+
     let comList = Object.values(com)
    
     return (
         <div>
             {comList.map((item, idx) => (
                 <p key={'num' + idx + item.text}> 
-                    {item.user_id}
+                    <div style={idText}>{item.user_id}</div>
                     {item.text}
-                    <span style={smallText}> {item.time}</span>
+                    <span style={timeText}> {item.time}</span>
                 </p>
             ))}
         </div>
