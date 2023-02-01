6094160ed3a1110037d45a90f5a9c9f5c352029d
sungjae <sungjae@gmail.com>
2023-02-01 16:22:46 +0900

    sg

--- src/component/Chart/LFBarChart.js
+++ src/component/Chart/LFBarChart.js
@@ -1,17 +1,8 @@
 import React, { PureComponent } from 'react';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
-// requestbed(){
-//   const url ="http://localhost:8081/users/tfidf"
-//   const header = {"Content-type"}
-//   const data = {
-//     text : bed.state.id,
-//     value : bed.state.value
-//   }
-//   axios.get(url, data, header)
-
-// }
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+//import '../ApiService.js'
 
 const data = [
   {
@@ -20,46 +11,43 @@ const data = [
     pv: 2400,
     amt: 2400,
   },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
+ 
 ];
+const TFIDF_API_BASE_URL = "http://localhost:8081/users/tfidf";
+
+// async function test(){
+//   const test = await axios.get(TFIDF_API_BASE_URL)
+//   console.log("Test "+test)
+// }
 
 export default class Example extends PureComponent {
   static demoUrl = 'https://codesandbox.io/s/simple-bar-chart-tpz8r';
+  
+  //const TFIDF_API_BASE_URL = "http://localhost:8081/users/tfidf";
+  constructor(props) {
+    //현재 클래스형 컴포넌트가 상속받고 있는 
+    //리액트의 Component 클래스가 지닌 생성자 함수를 호출해 준다.
+    super(props);
+    //state의 초깃값 설정하기 
+    this.state = {
+        keyW : "",
+        value : "",
+        getData : async function(){
+          await axios.get(TFIDF_API_BASE_URL)
+        }
+    };
+}
+
+
+
+componentDidMount() {
+  // fetch(TFIDF_API_BASE_URL + "/")
+  // .then((res)=>{
+  //   console.log(res)
+  // })
+  this.state.getData()
+}
+
 
   render() {
     return (
