6de7b8049fed0cf8acc95303b13dff416cc82d44
handaechan <h33223012@naver.com>
2023-02-04 14:15:24 +0900

    0204

--- src/component/pageComponent/Test4.jsx
+++ src/component/pageComponent/Test4.jsx
@@ -10,15 +10,10 @@ import ScrollBottom from "./Dont_Touch/ScrollBottom";
 import { LeftCircleOutlined, RightCircleOutlined } from "@ant-design/icons";
 import Footer from "./Footer";
 import axios from "axios";
-
 // 등록
-
 class Test4 extends Component {
-
-
   constructor(props) {
     super(props);
-
     this.state = {
       test_seq: "",
       test_title: "",
@@ -36,10 +31,7 @@ class Test4 extends Component {
     };
   }
   componentDidMount() {
-    window.location.reload();
-
     console.log('category_seq',sessionStorage.getItem('category_seq'));
-
     let getUrl = sessionStorage.getItem("video_url");
     let test_title = sessionStorage.getItem('test_title');
     let test_photo = sessionStorage.getItem("photo");
@@ -50,7 +42,6 @@ class Test4 extends Component {
     let user_id = sessionStorage.getItem('info');
     let test_content = sessionStorage.getItem('test_content');
     let category_seq = sessionStorage.getItem('category_seq');
-    
     this.setState({video_url:getUrl});
     this.setState({test_title : test_title});
     this.setState({test_photo : test_photo});
@@ -75,44 +66,35 @@ class Test4 extends Component {
       user_id : this.state.user_id,
       category_seq : this.state.category_seq
     }
-
     axios.post('/test/test',test, {
-     
       baseURL: 'http://localhost:8081/users'
     })
       .then((response) => {
          //if(response.data == '/testlist'){
           //this.navigate('/testlist');
-
          //}
       })
       .catch((error) => {
         // 예외 처리
       })
   }
-
   render() {
     return (
       <div>
-
-
-
         <form>
-     
           <MainHeader />
-
           <div
             style={{
-              marginTop: "2vh",
+              marginTop: "10px",
               marginLeft: "19.5vw",
-              width: "31vw",
-              height: "5vh",
+              width: "27.5vw",
+              height: "10vh",
               paddingLeft: "10px",
               backgroundColor: "#404040",
               // borderRadius: 20,
               textAlign:'center',
               color:'white',
-              boxShadow :"5px 5px #b4b4b4"
+              boxShadow :"5px 5px #B4B4B4"
             }}
           >
             <div
@@ -123,10 +105,9 @@ class Test4 extends Component {
                 textShadow :"2px 2px 2px gray"
               }}
             >
-              <h1 style={{marginTop:'0vh'}}>모의 영상 생성</h1>
+              <h1>모의 영상 생성</h1>
             </div>
           </div>
-
           <div
             className="Chart01"
             style={{
@@ -135,13 +116,12 @@ class Test4 extends Component {
               marginTop: "10px",
               marginLeft: "20vw",
               backgroundColor: "white",
-
               borderRadius: 10,
             }}
           ><video src={this.state.video_url} controls autoPlay muted playsInline
           style={{
             width: "700px",
-            height: "40vh",
+            height: "50vh",
             marginTop: "10px",
             marginLeft: "-0.5vw",}}></video></div>
           <div
@@ -151,79 +131,60 @@ class Test4 extends Component {
               width: "700px",
               marginLeft: "19.5vw",
               backgroundColor: "#404040",
-              marginTop: "-50px",
+              marginTop: "20px",
               borderRadius :"10px",
-              boxShadow :"3px 3px #b4b4b4"
-              
+              boxShadow :"3px 3px #B4B4B4"
             }}
           >
-            <div className="nameSpace" style={{ marginTop: "-20px"}}>
-              <p style={{fontSize :"20px", fontWeight :"bold", color:'white', marginLeft:'1vw'}}>
-               {this.state.test_title}
+            <div className="nameSpace" style={{ marginTop: "-20px" , }}>
+              <p style={{fontSize :"24px", fontWeight :"bold", color:'white'}}>
+                [뷰티쓱세일]공동판매구역 랑콤편! 전 구매고객 럭키 박스 증정
               </p>
             </div>
-            <div className="userName" style={{fontSize : "16px", color:'white'}}>작성자 : {this.state.user_id}</div>
-            {/* <div
+            <div className="userName" style={{fontSize : "18px", color:'white'}}>작성자 : {this.state.user_id}</div>
+            <div
               className="telNumber"
               style={{ width: "20vw", marginLeft: "30%", marginTop: "-21px" ,fontSize : "18px", color:'white'}}
             >
               Tel : 000 - 1234 - 5678
-            </div> */}
+            </div>
             <div
               className="price"
               style={{ width: "20vw", marginLeft: "70%", marginTop: "-21px",fontSize : "18px", color:'white' }}
             >
               가격 : {this.state.test_price}
             </div>
-            </div>
           </div>
-
           <div className="Script">
             <div
               className="ScriptContents"
               style={{
                 width: "500px",
-                height: "464px",
+                height: "480px",
                 marginLeft: "1100px",
-                marginTop: "-570px",
+                marginTop: "-590px",
                 overflowY: "scroll",
-                backgroundColor: "white",
-                position :"relative",
-                border: "5px solid #404040",
-                
+                backgroundColor: "greenyellow",
               }}
             >{this.state.test_cuesheet}</div>
-            <div 
-            style={{ 
-              marginLeft: "1100px",
-              width:'26.5vw',
-              color:'white',
-              height:'4vh',
-              marginTop:'-1vh'
-             }}>
-              <div style={{backgroundColor:'#404040', height:'4vh'}}>
-              <h3 style={{padding:'5px'}}>1. 작성했던 스크립트와 영상을 확인해보세요.</h3>
-              </div>
-              <div style={{backgroundColor:'#404040', height:'4vh'}}>
-              <h3 style={{padding:'5px'}}>2. 모두 올바르게 작성되었다면 확인을 누르세요.</h3>
-              </div>
+            <div style={{ marginLeft: "1140px" }}>
+              <h3>1. 작성했던 스크립트와 영상을 확인해보세요.</h3>
+              <h3>2. 모두 올바르게 작성되었다면 확인을 누르세요.</h3>
             </div>
           </div>
-
-          <div style={{marginLeft :"66vw", marginTop:'7vh'}}>
+          <div style={{marginLeft :"1140px"}}>
             <form >
-            <input type="checkbox" value="agree" name="agreement1" style={{marginLeft:"10px"}}/>개인정보 동의 여부 
+            <input type="checkbox" value="agree" name="agreement1" style={{marginLeft:"10px"}}/>개인정보 동의하십니까
             <input type="checkbox" value="agree" name="agreement2" style={{marginLeft:"60px"}}/>영상 정보 공개
             </form>
           </div>
-
           <Link to={"/test3"}>
               <LeftCircleOutlined
                 style={{
                   position: "absolute",
                   bottom: "50px",
                   marginTop: "0px",
-                  left: "10vw",
+                  left: "14%",
                   fontSize: "300%",
                   color: "black",
                 }}
@@ -237,13 +198,12 @@ class Test4 extends Component {
               position: "absolute",
               bottom: "50px",
               marginTop: "0px",
-              right: "10vw",
+              right: "14%",
               fontSize: "300%",
               color: "black",
             }}
             />
             </Link>
-          
           <div className="testBtn" style={{ position: "absolute", bottom:"2vh", right:"40vw" }}>
           <Link to={"/test1"}>
             <div className='test1'
@@ -254,7 +214,6 @@ class Test4 extends Component {
                 borderBottom: "4px solid gray"
               }}></div>
           </Link>
-
           <Link to={"/test2"}>
             <div className='test2'
               style={{
@@ -264,7 +223,6 @@ class Test4 extends Component {
                 borderBottom: "4px solid gray"
               }}></div>
           </Link>
-
           <Link to={"/test3"}>
             <div className='test3'
               style={{
@@ -274,18 +232,16 @@ class Test4 extends Component {
                 borderBottom: "4px solid gray"
               }}></div>
           </Link>
-
           <Link to={"/test4"}>
             <div className='test4'
               style={{
                 width: "5vw",
                 marginLeft: "53vw",
                 marginTop: "-0.4vh",
-                borderBottom: "4px solid #0d0d73"
+                borderBottom: "4px solid #0D0D73"
               }}></div>
           </Link>
         </div>
-          
         </form>
         <Footer/>
       </div>
